78c88025e7fe03cd32db2e1954a368483824c5bf
joel <1666464510@qq.com>
2021-04-19 22:47:19 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>hi</div>
+  <Joel/>
+</template>
+
+<script>
+import Joel from './components/Joel.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Joel
+  }
+}
+</script>
---
+++ src/components/Joel.vue
@@ -0,0 +1,5 @@
+<template>
+<div>
+  测试组件
+</div>
+</template>
