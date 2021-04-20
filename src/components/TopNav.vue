af74feb5815fa56a81cb15687405d4d07b208e64
joel <1666464510@qq.com>
2021-04-20 21:34:44 +0800

    实现topNav响应式效果

--- src/components/TopNav.vue
+++ src/components/TopNav.vue
@@ -5,6 +5,7 @@
       <li>菜单1</li>
       <li>菜单2</li>
     </ul>
+    <span class="toggleAside"></span>
   </div>
 </template>
 
@@ -30,6 +31,8 @@ export default {
   padding: 16px;
   position: relative;
   z-index: 10;
+  justify-content: center;
+  align-items: center;
 
   > .logo {
     max-width: 6em;
@@ -43,6 +46,15 @@ export default {
 
     > li {
       margin: 0 1em;
+    }
+  }
+
+  @media (max-width: 500px) {
+    .menu {
+      display: none;
+    }
+    .logo {
+      margin: 0 auto;
     }
   }
 }
