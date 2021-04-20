c5b84e626216005ba2fb22130f7d461da0349e0e
joel <1666464510@qq.com>
2021-04-20 12:24:38 +0800

    初始化 aside 的css

--- src/views/Doc.vue
+++ src/views/Doc.vue
@@ -28,3 +28,22 @@ export default {
   components: {TopNav}
 }
 </script>
+
+<style lang="scss" scoped>
+aside {
+  background: lightblue;
+  width: 150px;
+  padding: 16px;
+  position: fixed;
+  top: 0;
+  left: 0;
+  > h2 {
+    margin-bottom: 4px;
+  }
+  > ol {
+    > li {
+      padding: 4px 0;
+    }
+  }
+}
+</style>
