31960487d30322b55d2f3c6d1ffa964867f1fde2
joel <1666464510@qq.com>
2021-04-22 12:40:01 +0800

    添加theme属性

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -5,6 +5,9 @@
   <h1>demo 1</h1>
   <div>
     <Button>hello</Button>
+    <Button theme="button">hello</Button>
+    <Button theme="link">hello</Button>
+    <Button theme="text">hello</Button>
   </div>
 </template>
 
@@ -12,7 +15,13 @@
 import Button from '../lib/Button.vue';
 
 export default {
-  components: {Button}
+  components: {Button},
+  setup() {
+    const onClick = () => {
+      console.log('你好');
+    };
+    return {onClick};
+  }
 };
 
 </script>
--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -1,5 +1,16 @@
 <template>
-  <button>
+  <button class="flash-button" :class="`theme-${theme}`">
     <slot/>
   </button>
 </template>
+
+<script lang="ts">
+export default {
+  props: {
+    theme: {
+      type: String,
+      default: 'button'
+    }
+  }
+};
+</script>
