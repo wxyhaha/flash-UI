fc17305f448a01ba4a2f7e0e6673ccf84f579f10
joel <1666464510@qq.com>
2021-04-22 11:56:37 +0800

    创建Button组件

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -2,4 +2,17 @@
   <div>
     Button demo
   </div>
+  <h1>demo 1</h1>
+  <div>
+    <Button>hello</Button>
+  </div>
 </template>
+
+<script lang="ts">
+import Button from '../lib/Button.vue';
+
+export default {
+  components: {Button}
+};
+
+</script>
