23e0fa17edcca5f860709cabb2bb7ba7068eee5f
joel <1666464510@qq.com>
2021-04-22 17:05:04 +0800

    支持level属性

--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -16,14 +16,19 @@ export default {
     size: {
       type: String,
       default: 'normal'
-    }
+    },
+    level: {
+      type: String,
+      default: 'normal',
+    },
   },
   setup(props) {
-    const {theme, size} = props;
+    const {theme, size, level} = props;
     const classes = computed(() => {
       return {
         [`flash-theme-${theme}`]: theme,
         [`flash-size-${size}`]: size,
+        [`flash-level-${level}`]: level,
       };
     });
     return {classes};
@@ -37,6 +42,7 @@ $border-color: #d9d9d9;
 $color: #333;
 $blue: #40a9ff;
 $radius: 4px;
+$red: red;
 .flash-button {
   box-sizing: border-box;
   height: $h;
@@ -51,6 +57,7 @@ $radius: 4px;
   border: 1px solid $border-color;
   border-radius: $radius;
   box-shadow: 0 1px 0 fade-out(black, 0.95);
+  transition: background 250ms;
 
   & + & {
     margin-left: 8px;
@@ -95,10 +102,67 @@ $radius: 4px;
       padding: 0 16px
     }
 
-    &.flash-size-small {
-      font-size: 12px;
-      height: 20px;
-      padding: 0 4px;
+  &.flash-size-small {
+    font-size: 12px;
+    height: 20px;
+    padding: 0 4px;
+  }
+
+  &.flash-theme-button {
+    &.flash-level-main {
+      background: $blue;
+      color: white;
+      border-color: $blue;
+
+      &:hover,
+      &:focus {
+        background: darken($blue, 10%);
+        border-color: darken($blue, 10%);
+      }
+    }
+
+    &.flash-level-danger {
+      background: $red;
+      border-color: $red;
+      color: white;
+
+      &:hover,
+      &:focus {
+        background: darken($red, 10%);
+        border-color: darken($red, 10%);
+      }
     }
   }
+
+  &.flash-theme-link {
+    &.flash-level-danger {
+      color: $red;
+
+      &:hover,
+      &:focus {
+        color: darken($red, 10%);
+      }
+    }
+  }
+
+  &.flash-theme-text {
+    &.flash-level-main {
+      color: $blue;
+
+      &:hover,
+      &:focus {
+        color: darken($blue, 10%);
+      }
+    }
+
+    &.flash-level-danger {
+      color: $red;
+
+      &:hover,
+      &:focus {
+        color: darken($red, 10%);
+      }
+    }
+  }
+}
 </style>
