bd76ed2ae2efd991834a69c3189f8e853e4fec3b
joel <1666464510@qq.com>
2021-04-22 13:54:30 +0800

    支持theme属性

--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -53,5 +53,25 @@ $radius: 4px;
   &::-moz-focus-inner {
     border: 0;
   }
+
+  &.flash-theme-link {
+    border-color: transparent;
+    box-shadow: none;
+    color: $blue;
+
+    &:hover, &:focus {
+      color: lighten($blue, 10%);
+    }
+  }
+
+  &.flash-theme-text {
+    border-color: transparent;
+    box-shadow: none;
+    color: inherit;
+
+    &:hover, &:focus {
+      background: darken(white, 5%);;
+    }
+  }
 }
 </style>
