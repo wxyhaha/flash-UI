1e276767a897647b6b932342e1b9763c68e92ca5
joel <1666464510@qq.com>
2021-04-23 12:35:00 +0800

    dialog支持点击关闭

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -2,7 +2,7 @@
   <div>Dialog 示例</div>
   <h1>示例 1</h1>
   <Button @click="toggle">toggle</Button>
-  <Dialog :visible="x"></Dialog>
+  <Dialog v-model:visible="x" :ok="f1" :cancel="f2"></Dialog>
 </template>
 
 <script lang="ts">
@@ -19,7 +19,12 @@ export default {
     const toggle = () => {
       x.value = !x.value;
     };
-    return {x, toggle};
+    const f1 = () => {
+      return true;
+    };
+    const f2 = () => {
+    };
+    return {x, toggle, f1, f2};
   }
 };
 </script>
