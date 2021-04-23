b40d818117c016dcf04bb1548c18889da9c42f82
joel <1666464510@qq.com>
2021-04-23 23:31:52 +0800

    优化代码一

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -4,7 +4,7 @@
       <div class="flash-tabs-nav-item"
            @click="select(t)"
            v-for="(t,index) in titles" :key="index"
-           :ref="el => { if (el) navItems[index] = el }"
+           :ref="el => { if (t===selected) selectedItem = el }"
            :class="{selected:t===selected}"
       >{{ t }}
       </div>
@@ -27,16 +27,14 @@ export default {
     }
   },
   setup(props, context) {
-    const navItems = ref<HTMLDivElement[]>([]);
+    const selectedItem = ref<HTMLDivElement>(null);
     const indicator = ref<HTMLDivElement>(null);
     const container = ref<HTMLDivElement>(null);
     const x = () => {
-      const divs = navItems.value;
-      const result = divs.filter(div => div.classList.contains('selected'))[0];
-      const {width} = result.getBoundingClientRect();
+      const {width} = selectedItem.value.getBoundingClientRect();
       indicator.value.style.width = width + 'px';
       const {left: left1} = container.value.getBoundingClientRect();
-      const {left: left2} = result.getBoundingClientRect();
+      const {left: left2} = selectedItem.value.getBoundingClientRect();
       const left = left2 - left1;
       indicator.value.style.left = left + 'px';
     };
@@ -60,7 +58,7 @@ export default {
     const select = (title: string) => {
       context.emit('update:selected', title);
     };
-    return {defaults, titles, current, select, navItems, indicator, container};
+    return {defaults, titles, current, select, selectedItem, indicator, container};
   }
 };
 </script>
