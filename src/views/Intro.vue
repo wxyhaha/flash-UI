af445a2cb926f9ee836e154027fe8b11b63e3a22
joel <1666464510@qq.com>
2021-04-24 23:24:27 +0800

    支持markdown文件

--- src/views/Intro.vue
+++ src/views/Intro.vue
@@ -1,7 +1,13 @@
 <template>
-  <article class="markdown-body">
-    <h1>介绍</h1>
-    <p>flash UI 是一款基于 Vue 3 和 TypeScript 的 UI 组件库。</p>
-    下一节：<a href="#/doc/install">安装</a>
+  <article class="markdown-body" v-html="md">
   </article>
 </template>
+<script lang="ts">
+import md from '../markdown/intro.md';
+
+export default {
+  data() {
+    return {md};
+  }
+};
+</script>
