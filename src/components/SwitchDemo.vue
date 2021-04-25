5a405af8e391522409c5cf105c2a994af9f47fb7
joel <1666464510@qq.com>
2021-04-25 14:38:39 +0800

    删除冗余代码

--- src/components/SwitchDemo.vue
+++ src/components/SwitchDemo.vue
@@ -7,21 +7,14 @@
 </template>
 
 <script lang="ts">
-import {ref} from 'vue';
-import Button from '../lib/Button.vue';
 import Switch1Demo from './Switch1.demo.vue';
 import Switch2Demo from './Switch2.demo.vue';
-import 'prismjs';
-import 'prismjs/themes/prism-okaidia.css';
 import Demo from './Demo.vue';
 
-const Prism = (window as any).Prism;
-
 export default {
-  components: {Demo, Button},
+  components: {Demo},
   setup() {
-    const bool = ref(false);
-    return {bool, Switch1Demo, Switch2Demo, Prism};
+    return {Switch1Demo, Switch2Demo};
   }
 };
 </script>
