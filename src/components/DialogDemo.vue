523d029e4edbf3beb616d31e445669cb7fdabb2c
joel <1666464510@qq.com>
2021-04-25 15:39:05 +0800

    改造 DialogDemo

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -1,54 +1,19 @@
 <template>
   <div>Dialog 示例</div>
-  <h1>示例 1</h1>
-  <Button @click="toggle">toggle</Button>
-  <Dialog v-model:visible="x" :ok="f1" :cancel="f2">
-    <template v-slot:content>
-      <div>你好</div>
-      <div>hello</div>
-    </template>
-    <template v-slot:title>
-      <strong>加粗的标题</strong>
-    </template>
-  </Dialog>
-  <h1>示例 2</h1>
-  <Button @click="showDialog">show</Button>
+  <Demo :component="Dialog1Demo"/>
+  <Demo :component="Dialog2Demo"/>
 
 </template>
 
 <script lang="ts">
-import Dialog from '../lib/Dialog.vue';
-import Button from '../lib/Button.vue';
-import {ref} from 'vue';
-import {openDialog} from '../lib/openDialog';
+import Demo from './Demo.vue';
+import Dialog1Demo from './DialogDemos/Dialog1.demo.vue';
+import Dialog2Demo from './DialogDemos/Dialog2.demo.vue';
 
 export default {
-  components: {
-    Dialog, Button
-  },
+  components: {Demo},
   setup() {
-    const x = ref(false);
-    const toggle = () => {
-      x.value = !x.value;
-    };
-    const f1 = () => {
-      return true;
-    };
-    const f2 = () => {
-    };
-    const showDialog = () => {
-      openDialog({
-        title: '标题',
-        content: '你好',
-        ok() {
-          console.log('ok');
-        },
-        cancel() {
-          console.log('cancel');
-        }
-      });
-    };
-    return {x, toggle, f1, f2, showDialog};
+    return {Dialog1Demo, Dialog2Demo};
   }
 };
 </script>
---
+++ src/components/DialogDemos/Dialog1.demo.vue
@@ -0,0 +1,43 @@
+<demo>
+常规使用
+</demo>
+<template>
+  <div>
+    <Button @click="toggle">toggle</Button>
+    <Dialog v-model:visible="x" :ok="f1" :cancel="f2">
+      <template v-slot:content>
+        <div>你好</div>
+        <div>hello</div>
+      </template>
+      <template v-slot:title>
+        <strong>加粗的标题</strong>
+      </template>
+    </Dialog>
+  </div>
+</template>
+<script>
+import Button from "../../lib/Button.vue";
+import Dialog from "../../lib/Dialog.vue";
+import {ref} from "vue";
+
+export default {
+  components: {Dialog, Button},
+  setup() {
+    const x = ref(false);
+    const toggle = () => {
+      x.value = !x.value;
+    };
+    const f1 = () => {
+      return true;
+    };
+    const f2 = () => {
+    };
+    return {
+      x,
+      toggle,
+      f1,
+      f2
+    }
+  }
+}
+</script>
